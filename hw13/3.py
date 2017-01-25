bffd31d77546460fddfcfe0d3f19fbe66198acac
Flying Bear <Galka1999@gmail.com>
2017-01-26 01:07:12 +0300

    Homework 13 added
    Cannot capture future forms

---
+++ hw13/3.py
@@ -0,0 +1,65 @@
+##3. В этом домашнем задании программа должна открывать файл с русским текстом в кодировке UTF-8 и
+##распечатывать из него по одному разу все встретившиеся в нём формы глагола "программировать".
+##В формы глагола включаются деепричастия, причастия и формы на -ся и не включаются видовые пары
+##(тем более что не у всех из перечисленных глаголов они имеются).
+##И особое внимание стоит уделить тому, чтобы программа ничего, кроме форм этих глаголов, не распознавала.
+import re
+
+
+def match_verb_forms(line):
+    future = re.match(r'буд(е(шь|те?|м)|ут?) программировать', line, re.I)
+    present = re.match(r'программиру(ю|(е(те?|м|шь)))', line, re.I)
+    past = re.match(r'программировал(а|и)?', line, re.I)
+    infinitive = re.match(r'программировать(ся)?', line, re.I)
+    past_participle = re.match(r'программированн(ая|о(е|й|му?|го)|ы(й|е|ми?|х))', line, re.I)
+    present_participle = re.match(r'программируем(ая|о(е|й|му?|го)|ы(й|е|ми?|х))', line, re.I)
+    transgressive_active = re.match(r'программируя', line, re.I)
+    transgressive_passive_past = re.match(r'будучи программированн(ая|о(е|й|му?|го)|ы(й|е|ми?|х))', line, re.I)
+    transgressive_passive_present = re.match(r'будучи программируем(ая|о(е|й|му?|го)|ы(й|е|ми?|х))', line, re.I)
+    if future:
+        match = future
+    elif present:
+        match = present
+    elif past:
+        match = past
+    elif infinitive:
+        match = infinitive
+    elif past_participle:
+        match = past_participle
+    elif present_participle:
+        match = present_participle
+    elif transgressive_active:
+        match = transgressive_active
+    elif transgressive_passive_past:
+        match = transgressive_passive_past
+    elif transgressive_passive_present:
+        match = transgressive_passive_present
+    else:
+        match = None
+    return match
+
+def open_forms(fname):
+    forms = []
+    with open (fname, 'r', encoding = 'utf-8') as f:
+        text = f.read()
+    text = text.lower()
+    forms = text.split()
+    for i in range(len(forms)):
+        forms[i] = forms[i].strip('.,?*()«»')
+    return forms
+
+def main():
+    matches = []
+    forms = open_forms('test.txt')
+    for i in range(len(forms)-1):
+        if i < len(forms):
+            if match_verb_forms(forms[i] + forms[i+1]) and match_verb_forms(forms[i] + forms[i+1]) not in matches:
+                matches.append(match_verb_forms(forms[i] + forms[i+1]).group(0))
+        else:
+            if match_verb_forms(forms[i]) and match_verb_forms(forms[i]) not in matches:
+                matches.append(match_verb_forms(forms[i]).group(0))
+    print(matches)
+            
+    
+if __name__ == '__main__':
+    main()
