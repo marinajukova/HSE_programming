f1dd5407beaeadc1bd20ccbbfe29aaeaae7d312d
Flying Bear <Galka1999@gmail.com>
2017-02-13 14:15:48 +0300

    homework about abrakadabra renamed to hw4

--- hw4/3.py
+++ hw4/3.py
@@ -1,15 +1,17 @@
-## 3. Спрашивает у пользователя слово
-## и выводит на экран все буквы этого слова задом наперёд, пропуская буквы "з" и "я".
+##3 вариант
+##abracadabra
+##bracadabraa
+##racadabraab
+##acadabraabr
+##cadabraabra
+##adabraabrac
+##dabraabraca
+##abraabracad
+##braabracada
+##raabracadab
+##aabracadabr
 
-##word = input('Введите слово в русской раскладке.')
-##for index in range(len(word)):
-##    if word[len(word) - index - 1] != 'з' and word[len(word) - index - 1] != 'я':
-##        print(word[len(word) - index - 1])
-        
-word = input('Введите слово в русской раскладке.')
-index = 0
-while index < len(word):
-    index += 1
-    if word[len(word) - index] != 'з' and word[len(word) - index] != 'я':
-        print(word[len(word) - index])
+text = input('Type something: ')
+for i in range(len(text)):
+    print(text[i:]+text[:i])
 
