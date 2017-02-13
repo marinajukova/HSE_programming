04e9de2bc75151a9e3088319dec430623866c445
Flying Bear <Galka1999@gmail.com>
2017-02-13 20:52:04 +0300

    pretest dictionaries 1 updated

--- pretest/dictionaries/1.py
+++ pretest/dictionaries/1.py
@@ -2,12 +2,18 @@
 ## а частотность в качестве значений. После этого нужно распечатать самое частотное слово в тексте.
 ## Найти среднее значение частотности слов в тексте и распечатать его.
 
-def freqlist(text):
-    freqs = {}
+def process(fname):
+    with open (fname, 'r', encoding = 'utf-8') as f:
+        text = f.read()
     text = text.lower()
     forms = text.split()
     for i in range(len(forms)):
-        forms[i] = forms[i].strip('.,!?*()«»\'"')
+        forms[i] = forms[i].strip('.,!?*()«»\'":][><')
+    return forms
+
+
+def freqlist(forms):
+    freqs = {}
     for i in range(len(forms)):
         if forms[i] not in freqs:
             freqs[forms[i]] = 1
@@ -15,27 +21,28 @@ def freqlist(text):
             freqs[forms[i]] +=1
     return freqs
 
-def maxfreq(text):
-    frequencies = freqlist(text)
+
+def maxfreq(frequencies):
     maximumfreq = []
     for key in frequencies:
         if frequencies[key] == max(frequencies.values()):
             maximumfreq.append(key)
     return maximumfreq
 
-def averagefreq(text):
-    frequencies = freqlist(text)
+
+def averagefreq(frequencies):
     total = 0
     for key in frequencies:
         total += frequencies[key]
     average = total/len(frequencies)
     return average
 
+
 def main():
-    with open ('text.txt', 'r', encoding = 'utf-8') as f:
-        text = f.read()
-    print(*maxfreq(text), '- самое частотное слово в тексте.')
-    print(averagefreq(text), '- средняя частота слов в тексте.')
+    forms = process('text.txt')
+    frequencies = freqlist(forms)
+    print(*maxfreq(frequencies), '- самое частотное слово в тексте.')
+    print(averagefreq(frequencies), '- средняя частота слов в тексте.')
 
 if __name__ == '__main__':
     main()
