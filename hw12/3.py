f17e139827a891153b84969915aa3e58f61ca922
Flying Bear <Galka1999@gmail.com>
2017-04-06 16:28:04 +0300

    First version of hw12 added

---
+++ hw12/3.py
@@ -0,0 +1,32 @@
+## 3. Нужно взять какой-нибудь файл с достаточно большим текстом, прочитать его,
+## поделить на предложения (просто по знакам конца предложения), удалить знаки препинания.
+## Затем сделать следующее (обязательно использовать list comprehensions и formatting):
+## Преобразовать все предложения в тексте, после каждого слова дописав его длину в символах через подчеркивание. Так, "Мама вымыла раму" превращается в 
+## Мама_4 
+## вымыла_6 
+## раму_4
+
+
+import re
+
+
+def open_text_phrases(fname):
+    phrases = []
+    with open (fname, 'r', encoding = 'utf-8') as f:
+        text = f.read()
+    text = re.sub('\.\.\.|[\.\?]', '!', text)
+    phrases = text.split('!')[:-1]
+    for i in range(len(phrases)):
+         phrases[i] = re.sub('[<>\*\.«»,\'\"]','', phrases[i])
+         phrases[i] = phrases[i].strip()
+    return phrases
+
+
+def main():
+    phrase_list = open_text_phrases('text.txt')
+    word_length_list = [w+'_'+str(len(w)) for phrase in phrase_list for w in phrase.split()]
+    for word in word_length_list:
+        print(word)
+
+if __name__ == '__main__':
+    main()
